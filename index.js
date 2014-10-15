4eabe30bf0a9d074e7e1e5788bb1f774d27dcecd
Efremov Alexey <lexich121@gmail.com>
2014-10-15 10:15:45 +0400

    init

---
+++ index.js
@@ -0,0 +1,90 @@
+"use strict";
+//{ format: 'png', width: 400, height: 300, file: '2.png' }
+var nodecss = require("node-css"),
+    libpath = require("path"),
+    CSS = nodecss.CSS;
+
+var MEDIA_QUERY = "(min-device-pixel-ratio: 2) and (min-resolution: 192dpi)";
+var css = new CSS();
+function CSSImage(){
+
+}
+
+CSSImage.prototype.css = function(filepath, width, height, root, options){
+  var classname = "." + this.name(filepath);
+  if(options && options.is_retina){
+    options.media = MEDIA_QUERY;
+  }
+  return css.statement(classname,{
+    width: width + "px",
+    height: height + "px",
+    "background-image": this.url(filepath, root),
+    "background-size": "" + width  + "px " + height + "px"
+  }, options);
+};
+
+CSSImage.prototype.scss_vars = function(filepath, width, height){
+  var name = this.name(filepath);
+  return "$" + name + "__width: " + width + "px\n" +
+         "$" + name + "__height: " + height + "px\n";
+};
+
+CSSImage.prototype.scss_mixin = function(filepath, width, height, root, options){
+  var name = this.name(filepath);
+  var classname = "@mixin " + name + "()";
+  var is_retina = options && options.is_retina;
+  var indent = "";
+  if(is_retina){
+    indent = css.options(options, "indent");
+    options.indent = indent + indent;
+  }
+  var body = css.body({
+    width: width + "px",
+    height: height + "px",
+    "background-image": this.url(filepath, root),
+    "background-size": "" + width  + "px " + height + "px"
+  }, options);
+  if(is_retina){
+    body = indent + "@media " + MEDIA_QUERY + "{\n" + body + indent + "}\n";
+  }
+  return classname + "{\n" + body + "}\n";
+};
+
+CSSImage.prototype.url = function(filepath, root){
+  return "url(" + this.normalize_path(filepath, root) + ")";
+};
+
+CSSImage.prototype.normalize_path = function(filepath, root){
+  return libpath.join(
+    this.normalize_folder(filepath, root),
+    libpath.basename(filepath)
+  );
+};
+
+CSSImage.prototype.normalize_folder = function(filepath, root){
+  var folder = libpath.dirname(filepath);
+  if(folder[0] === "." && folder[1] !== "."){ folder = folder.slice(1); }
+  if(folder[0] === "/"){ folder = folder.slice(1); }
+  if(root){
+    return libpath.join(root, folder);
+  } else {
+    return folder;
+  }
+};
+
+CSSImage.prototype.name = function(filepath){
+  var filename = libpath.basename(filepath);
+  var ext = libpath.extname(filepath);
+  var name = filename.slice(0, filename.length - ext.length).replace(/\./,"");
+  var folder = this.normalize_folder(filepath);
+  if(folder === ""){ return name; }
+  return folder.replace(/\//g, "_") + "_" + name;
+};
+
+
+function cssimage(){
+
+}
+
+module.exports = cssimage;
+module.exports.CSSImage = CSSImage;
---
+++ test/main.js
@@ -0,0 +1,101 @@
+/*global describe, it, xit*/
+/* jshint expr: true */
+"use strict";
+
+var should = require("should"),
+    cssimage = require("../");
+
+describe("test CSSImage", function(){
+  var CSSImage = cssimage.CSSImage;
+  var c = new CSSImage();
+  it("test name", function(){
+    c.name("2.png").should.eql("2");
+    c.name("test/2.jpg").should.eql("test_2");
+    c.name("/images/test/2.gif").should.eql("images_test_2");
+    c.name("./images/test/2.gif").should.eql("images_test_2");
+    c.name(".images/test/2.gif").should.eql("images_test_2");
+    c.name("images/test/.2.jpg").should.eql("images_test_2");
+  });
+  it("test normalize_folder", function(){
+    c.normalize_folder(".").should.eql("");
+    c.normalize_folder("test.jpg").should.eql("");
+    c.normalize_folder("/test.jpg").should.eql("");
+    c.normalize_folder("./test.jpg").should.eql("");
+    c.normalize_folder(".test.jpg").should.eql("");
+    c.normalize_folder("test/test1.jpg").should.eql("test");
+    c.normalize_folder("../images/test").should.eql("../images");
+  });
+  it("test normalize_folder with root", function(){
+    c.normalize_folder(".", "images").should.eql("images");
+    c.normalize_folder("test/test.jpg", "images").should.eql("images/test");
+    c.normalize_folder("/test.jpg", "images/root").should.eql("images/root");
+    c.normalize_folder("./test.jpg", "images").should.eql("images");
+    c.normalize_folder(".test.jpg", "root").should.eql("root");
+    c.normalize_folder("../images/test.jpg", "root").should.eql("images");
+    c.normalize_folder("images/test.jpg", "../root").should.eql("../root/images");
+  });
+  it("test normalize_path", function(){
+    c.normalize_path(".").should.eql(".");
+    c.normalize_path("test.jpg").should.eql("test.jpg");
+    c.normalize_path("/test.jpg").should.eql("test.jpg");
+    c.normalize_path("./test.jpg").should.eql("test.jpg");
+    c.normalize_path(".test.jpg").should.eql(".test.jpg");
+    c.normalize_path("test/test1.jpg").should.eql("test/test1.jpg");
+  });
+  it("test normalize_path with root", function(){
+    c.normalize_path(".", "images").should.eql("images");
+    c.normalize_path("test/test.jpg", "images").should.eql("images/test/test.jpg");
+    c.normalize_path("/test.jpg", "images/root").should.eql("images/root/test.jpg");
+    c.normalize_path("./test.jpg", "images").should.eql("images/test.jpg");
+    c.normalize_path(".test.jpg", "root").should.eql("root/.test.jpg");
+  });
+  it("test url", function(){
+    c.url("test.jpg", "../images").should.eql("url(../images/test.jpg)");
+  });
+  it("test css", function(){
+    var result = ".test_images{\n" +
+                 "  width: 100px;\n" +
+                 "  height: 110px;\n" +
+                 "  background-image: url(../images/test/images.jpg);\n" +
+                 "  background-size: 100px 110px;\n" +
+                 "}\n";
+    c.css("test/images.jpg", 100, 110, "../images").should.eql(result);
+  });
+  it("test css with retina", function(){
+    var result = "@media (min-device-pixel-ratio: 2) and (min-resolution: 192dpi){\n" + 
+                 "  .test_images{\n" +
+                 "    width: 50px;\n" +
+                 "    height: 55px;\n" +
+                 "    background-image: url(../images/test/images.jpg);\n" +
+                 "    background-size: 50px 55px;\n" +
+                 "  }\n" +
+                 "}\n";
+    c.css("test/images.jpg", 50, 55, "../images", {is_retina: true}).should.eql(result);
+  });
+
+  it("test scss_mixin with retina", function(){
+    var result = "@mixin test_images(){\n" + 
+                 "  @media (min-device-pixel-ratio: 2) and (min-resolution: 192dpi){\n" +
+                 "    width: 100px;\n" +
+                 "    height: 110px;\n" +
+                 "    background-image: url(../images/test/images.jpg);\n" +
+                 "    background-size: 100px 110px;\n" +
+                 "  }\n" + 
+                 "}\n";
+    c.scss_mixin("test/images.jpg", 100, 110, "../images", {is_retina: true}).should.eql(result);
+  });
+  it("test scss_mixin", function(){
+    var result = "@mixin test_images(){\n" + 
+                 "  width: 100px;\n"+
+                 "  height: 110px;\n"+
+                 "  background-image: url(../images/test/images.jpg);\n"+
+                 "  background-size: 100px 110px;\n"+
+                 "}\n";
+    c.scss_mixin("test/images.jpg", 100, 110, "../images").should.eql(result);
+  });
+  it("test scss_vars", function(){
+    var result = "$test_images__width: 100px\n" + 
+                 "$test_images__height: 110px\n";
+    c.scss_vars("test/images.jpg", 100, 110).should.eql(result);
+  });
+});
